36b810f01c458cc3f1d7bb5a37fb1f7557c6c8fe
kabuspl <59837830+kabuspl@users.noreply.github.com>
2021-07-20 10:12:40 +0200

    fix intelligent selection

--- mckielce.js
+++ mckielce.js
@@ -161,10 +161,9 @@ window.onload = function() {
     let imgurl = "http://noproxy.kabus.ga:2139/?&service=WMS&request=GetMap&layers=Raster&styles=&format=image%2Fjpeg&transparent=false&version=1.1.1&width=256&height=256&srs=EPSG%3A3857&bbox=2304117.7806283534,6584591.364598226,2309009.7504386045,6589483.334408476";
     let img2 = new Image();
     img2.src=imgurl;
-    img2.addEventListener("error",() => {
-        layers.addBaseLayer(geoportal_fallback,"Geoportal.gov.pl (Fallback)");
-    });
+    let timer = setTimeout(()=>{layers.addBaseLayer(geoportal_fallback,"Geoportal.gov.pl (Fallback)"); img2.remove()},5000);
     img2.addEventListener("load",() => {
+        clearTimeout(timer);
         layers.addBaseLayer(geoportal,"Geoportal.gov.pl");
     });
     
